a5a7cd269877565762281a32baba272ebdbc12ba
Musharab Sabeen <musharabsabeen@gmail.com>
2026-06-03 13:21:21 +0500

    optimize middleware matcher to prevent timeout

--- middleware.ts
+++ middleware.ts
@@ -30,5 +30,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|icon-192.png|icon-512.png|manifest.json|.*\\.svg$|.*\\.png$|.*\\.ico$).*)',
+  ],
 }
